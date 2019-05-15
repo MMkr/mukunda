c993e34a089832ab125d1a6e45c1c1cbf8828b2d
MMkr <50393559+MMkr@users.noreply.github.com>
2019-05-16 00:05:10 +0530

    Update multichain.js

--- multichain.js
+++ multichain.js
@@ -2,7 +2,7 @@ let multichain = require("multichain-node")({
     port: 6468,
     host: '127.0.0.1',
     user: "multichainrpc",
-    pass: "DyJm1SRztnVMpDWQ6S3bDHiK2Qb9nnNPyFuiBpc2ZU3i"
+    pass: "xxxxxxxxxxxxxxxxxxxxxxxxx"
 });
 
 module.exports = multichain;
